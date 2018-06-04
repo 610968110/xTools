708e8784011ef144d73bd918309d01f35b7406fe
610968110 <610968110@qq.com>
2018-06-04 10:57:38 +0800

    fix bug

--- xtoollib/src/main/java/lbx/xtoollib/bean/Result.java
+++
@@ -1,9 +0,0 @@
-package lbx.xtoollib.bean;
-
-/**
- * @author lbx
- * @date 2018/5/4.
- */
-
-public interface Result {
-}
---
+++ xtoollib/src/main/java/lbx/xtoollib/impl/ForApp.java
@@ -0,0 +1,9 @@
+package lbx.xtoollib.impl;
+
+/**
+ * @author lbx
+ * @date 2018/5/31.
+ */
+
+public class ForApp {
+}
