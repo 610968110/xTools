e11d1e21649becfd56ca5962afe982a42d1bd996
610968110 <610968110@qq.com>
2018-06-06 11:17:48 +0800

    fix web bugs

---
+++ xtoollib/src/main/java/lbx/xtoollib/listener/BaseAppBarStateChangeListener.java
@@ -0,0 +1,55 @@
+package com.telecomyt.lzpolice.listener;
+
+import android.support.design.widget.AppBarLayout;
+
+/**
+ * @author lbx
+ *         AppBarLayout 开/管回调
+ */
+public abstract class BaseAppBarStateChangeListener implements AppBarLayout.OnOffsetChangedListener {
+
+    public enum State {
+        /**
+         * 开
+         */
+        EXPANDED,
+        /**
+         * 关
+         */
+        COLLAPSED,
+        /**
+         * 中间状态
+         */
+        IDLE
+    }
+
+    /**
+     * 当前状态
+     */
+    private State mCurrentState = State.IDLE;
+
+    @Override
+    public final void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+        if (i == 0) {
+            if (mCurrentState != State.EXPANDED) {
+                onStateChanged(appBarLayout, State.EXPANDED, i);
+            }
+            mCurrentState = State.EXPANDED;
+        } else if (Math.abs(i) >= appBarLayout.getTotalScrollRange()) {
+            if (mCurrentState != State.COLLAPSED) {
+                onStateChanged(appBarLayout, State.COLLAPSED, i);
+            }
+            mCurrentState = State.COLLAPSED;
+        } else {
+            if (mCurrentState != State.IDLE) {
+                onStateChanged(appBarLayout, State.IDLE, i);
+            }
+            mCurrentState = State.IDLE;
+        }
+        onScrollChanged(appBarLayout, i);
+    }
+
+    public abstract void onStateChanged(AppBarLayout appBarLayout, State state, int i);
+
+    public abstract void onScrollChanged(AppBarLayout appBarLayout, int i);
+}
